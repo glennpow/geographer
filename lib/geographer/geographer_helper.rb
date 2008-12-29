6637fb472b339321d0f7040414f788d7dafe379e
glennpow <glennpow@gmail.com>
2008-12-29 12:27:56 -0800

    Updated helper to accept :mappable param

--- lib/geographer/geographer_helper.rb
+++ lib/geographer/geographer_helper.rb
@@ -11,6 +11,13 @@ module GeographerHelper
         options[:markers] << Geographer::Marker.new(:position => [ geo.lat, geo.lng ], :title => options[:query])
       end
     end
+    if options[:mappable]
+      options[:markers] ||= []
+      options[:markers] << Geographer::Marker.new(
+        :position => [ options[:mappable].send(options[:mappable].lat_column_name),
+          options[:mappable].send(options[:mappable].lng_column_name) ],
+        :title => options[:query])
+    end
     Geographer::Map.new(options).to_html
   end
 end
