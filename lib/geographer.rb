029e9955dcdad6a0ce66a53fa7255d651279b33b
glennpow <glennpow@gmail.com>
2008-12-31 09:51:18 -0800

    Fixed helper

--- lib/geographer.rb
+++ lib/geographer.rb
@@ -3,3 +3,6 @@ require File.dirname(__FILE__) + '/geographer/icon'
 require File.dirname(__FILE__) + '/geographer/map'
 require File.dirname(__FILE__) + '/geographer/marker'
 require File.dirname(__FILE__) + '/geographer/polyline'
+require File.dirname(__FILE__) + '/geographer/geographer_helper'
+
+ActionView::Base.send :include, GeographerHelper
